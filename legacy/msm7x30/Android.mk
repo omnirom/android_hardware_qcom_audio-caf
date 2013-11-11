6f5078140ae65416e146c6c550ca4c0037fe0e48
Michael Bestas <mikeioannina@gmail.com>
2013-11-11 03:36:36 +0200

    legacy/msm7x30: Fix stereo recording on SEMC devices
    * Finally, after 3 years. Thanks to miniuser123@xda for pointing the correct devices
    
    Change-Id: I1f4d59004a7d56aab62860b5c77e3bd8e95a677c

--- legacy/msm7x30/Android.mk
+++ legacy/msm7x30/Android.mk
@@ -25,6 +25,10 @@ ifeq ($(BOARD_USES_STEREO_HW_SPEAKER),true)
     LOCAL_CFLAGS += -DWITH_STEREO_HW_SPEAKER
 endif
 
+ifeq ($(BOARD_HAVE_SEMC_AUDIO),true)
+    LOCAL_CFLAGS += -DSEMC_AUDIO
+endif
+
 LOCAL_SRC_FILES := \
     AudioHardware.cpp \
     audio_hw_hal.cpp
