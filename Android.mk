9c1cc8b2d66403b3e24ff7c46b1998c5e814c0c2
Jon Haus <haus.xda@gmail.com>
2013-11-15 18:41:38 -0500

    audio-caf: redundant ifeq is redundant
    Removed duplicate ifeq check
    
    Change-Id: Ic30e66b87e9600b130b8286a48b54da17cc4b9a0

--- Android.mk
+++ Android.mk
@@ -3,19 +3,18 @@ ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 
 AUDIO_HW_ROOT := $(call my-dir)
 
-ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
-
 ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
     include $(AUDIO_HW_ROOT)/alsa_sound/Android.mk
     include $(AUDIO_HW_ROOT)/libalsa-intf/Android.mk
-	include $(AUDIO_HW_ROOT)/audiod/Android.mk
+    include $(AUDIO_HW_ROOT)/audiod/Android.mk
 endif
+
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
     include $(AUDIO_HW_ROOT)/msm7x30/Android.mk
 endif
+
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     include $(AUDIO_HW_ROOT)/msm8660/Android.mk
-endif
 endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
