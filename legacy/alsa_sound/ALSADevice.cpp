8dcdaa663dd757bf257d228fffc1f282cacc40ef
Steve Kondik <shade@chemlab.org>
2014-01-05 04:43:04 -0800

    alsa_sound: Raise priority of separate VOIP input
     * This needs to come first for devices that use this path, otherwise
       we fallthru to the standard voice call route.
     * Fixes Hangouts on JF.
    
    Change-Id: I21fbdf520a5bb0e48b3c8988bc99c9cca0b64e85

--- legacy/alsa_sound/ALSADevice.cpp
+++ legacy/alsa_sound/ALSADevice.cpp
@@ -1905,6 +1905,16 @@ char* ALSADevice::getUCMDevice(uint32_t devices, int input, char *rxDevice)
             if (!strncmp(mMicType, "analog", 6)) {
                 return strdup(SND_USE_CASE_DEV_HANDSET); /* HANDSET TX */
             } else {
+#ifdef SEPERATED_VOIP
+                if (mCallMode == AUDIO_MODE_IN_COMMUNICATION) {
+                    if (!strncmp(rxDevice, SND_USE_CASE_DEV_VOIP_EARPIECE,
+                                (strlen(SND_USE_CASE_DEV_VOIP_EARPIECE)+1))) {
+                        return strdup(SND_USE_CASE_DEV_VOIP_HANDSET);
+                    } else {
+                        return strdup(SND_USE_CASE_DEV_VOIP_LINE);
+                    }
+#endif
+
                 if ((mDevSettingsFlag & DMIC_FLAG) && (mInChannels == 1)) {
 #ifdef USES_FLUENCE_INCALL
                   if (mCallMode == AUDIO_MODE_IN_CALL
@@ -2013,17 +2023,6 @@ char* ALSADevice::getUCMDevice(uint32_t devices, int input, char *rxDevice)
                     return strdup(SND_USE_CASE_DEV_VOICE_RECOGNITION ); /* VOICE RECOGNITION TX */
                 }
 #endif
-#ifdef SEPERATED_VOIP
-                if (mCallMode == AUDIO_MODE_IN_COMMUNICATION) {
-                    if (!strncmp(rxDevice, SND_USE_CASE_DEV_VOIP_EARPIECE,
-                                (strlen(SND_USE_CASE_DEV_VOIP_EARPIECE)+1))) {
-                        return strdup(SND_USE_CASE_DEV_VOIP_HANDSET);
-                    } else {
-                        return strdup(SND_USE_CASE_DEV_VOIP_LINE);
-                    }
-                }
-#endif
-                else {
                     if ((rxDevice != NULL) &&
                         !strncmp(rxDevice, SND_USE_CASE_DEV_ANC_HANDSET,
                             strlen(SND_USE_CASE_DEV_ANC_HANDSET) + 1)) {
